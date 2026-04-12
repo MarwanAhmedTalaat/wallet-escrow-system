f0bc8318f9dec5621a8940d8cd538e7a6b685356
Marwan Talaat <marwantalaat650@gmail.com>
2026-04-12 21:39:15 +0200

    feat: add transaction model for credit and debit history

---
+++ src/modules/transaction/transaction.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+const transactionSchema = new mongoose.Schema({
+    walletId : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Wallet",
+        required :true
+    },
+    type : {
+        type : String,
+        enum : ["credit","debit"],
+        required :true
+    },
+    amount : {
+        type : Number,
+        required : true,
+    },
+    balanceAfter : {
+        type : Number,
+        required :true
+    }
+},{
+    timestamps: true
+})
+const Transaction = mongoose.model("Transaction",transactionSchema)
+module.exports = Transaction
